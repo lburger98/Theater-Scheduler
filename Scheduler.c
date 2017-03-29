8d0f4d0f79b9a4917ff7d080d03e769770789e82
lburger98 <lburger98@gmail.com>
2017-03-28 21:12:06 -0400

    comments and debugging

--- Scheduler.c
+++ Scheduler.c
@@ -4,6 +4,9 @@
 #include <stdbool.h>
 #include <assert.h>
 
+
+//create data structure for storing time (hours and minutes)
+
 struct time {
 int hour;
 int mins;
@@ -11,9 +14,11 @@ int mins;
 
 typedef struct time Time;
 
+//function for subtracting two time strucures
 Time subTime (Time t1, Time t2) {
   Time newtime;
   newtime.hour = t1.hour - t2.hour;
+  //carry over from the hours if the mins become negative
   if((t1.mins - t2.mins) < 0) {
     newtime.hour--;
     newtime.mins = 60 + (t1.mins - t2.mins);
@@ -23,6 +28,7 @@ Time subTime (Time t1, Time t2) {
   return newtime;
 }
 
+//function for comparing times: returns 1 if t2>t1 and 0 if t1>t2
 int compTime(Time t2, Time t1) {
    if (t2.hour == t1.hour) {
       return (t2.mins>t1.mins ? 1 : 0);
@@ -32,7 +38,7 @@ int compTime(Time t2, Time t1) {
 }
 
 void printTime(Time t) {
-
+//if mins are single digit add print a zero
   if(t.mins<10) {
   printf("%d:0%d", t.hour, t.mins);
   } else {
@@ -42,10 +48,11 @@ void printTime(Time t) {
 }
 
 int main(int argc, char **argv) {
+  //create time structures for when the theater opens, closes, and the time needed to clean
   Time interval = {0, 35};
   Time opentime;
   Time finalclosetime;
-
+  //search the arguments to set open and close times
   if(argc == 1) {
     printf("Usage: -monday -tuesday -wednesday etc.\n");
     return 0;
@@ -68,6 +75,7 @@ int main(int argc, char **argv) {
     }
   }
 
+  //capture the file from standard input into a string
   char *input;
   int size = 1024;
   input = malloc(size*sizeof(char));
@@ -88,6 +96,7 @@ int main(int argc, char **argv) {
   }
   input[i] = '\0';
 
+  //create an array of strings for storing tokens
   char *movies[(linecount-1)][4];
 
   const char s[4]= ",\n";
@@ -106,37 +115,51 @@ int main(int argc, char **argv) {
   }
 
 const char d[2] = ":\n";
+//cycle through movies
+for(k = 0; k < (linecount-1); k++) {
 
-for(int w = 0; w < (k+1); w++) {
+  //cycle through each token of the movie
   for(j=0; j<4; j++) {
-  if(j==3){
-    printf("%s\n", movies[w][j]);
-    Time runtime;
-    char *token2 = strtok(movies[w][j], d);
-    runtime.hour = atoi(token2);
-    token2 = strtok(NULL, d);
-  if(token2 != NULL) {
-    runtime.mins = atoi(token2);
-    Time tmp;
-    Time closetime = finalclosetime;
-    while(compTime(subTime(closetime, runtime), opentime)) {
-      tmp = subTime(closetime, runtime);
-      if((tmp.mins % 5) != 0) {
-        Time z = {0, (tmp.mins % 5)};
-        tmp = subTime(tmp, z);
-        closetime = subTime(closetime, z);
-      }
-      printf("\t"); printTime(tmp); printf(" - "); printTime(closetime); printf("\n");
-      closetime = subTime(tmp, interval);
-    }
-  }
-  } else if (j==2){
-    printf(" - Rated%s,", movies[w][j]);
-  }else if(j==1){
-    continue;
-  } else {
-    printf("%s", movies[w][j]);
-    }
+
+      //if we reach the runtime token, parse it then create the schedule
+      if(j==3){
+        printf("%s\n", movies[k][j]);
+
+        //parsing
+        Time runtime;
+        char *token2 = strtok(movies[k][j], d);
+        runtime.hour = atoi(token2);
+        token2 = strtok(NULL, d);
+      if(token2 != NULL) {
+        runtime.mins = atoi(token2);
+        Time tmp;
+        Time endtime = finalclosetime;
+
+        //create the schedule
+        while(compTime(subTime(endtime, runtime), opentime)) {
+          tmp = subTime(endtime, runtime);
+          //make the starttimes a multiple of 5
+          if((tmp.mins % 5) != 0) {
+            Time z = {0, (tmp.mins % 5)};
+            tmp = subTime(tmp, z);
+            endtime = subTime(endtime, z);
+          }
+
+          printf("\t"); printTime(tmp); printf(" - "); printTime(endtime); printf("\n");
+          //take away time for cleaning
+          endtime = subTime(tmp, interval);
+        }
+        }
+
+        //print the rest of the info
+
+      } else if (j==2){
+        printf(" - Rated%s,", movies[k][j]);
+      }else if(j==1){
+        continue;
+      } else {
+        printf("%s", movies[k][j]);
+        }
 }
   printf("\n");
 }
